9758884ddf819f681ac0cfd76d9a69093bbbbf6f
hebibov666 <asuslap503@gmail.com>
2024-05-13 20:27:32 +0300

    Change design of Pagination.js component

--- src/components/spesific/Pagination.js
+++ src/components/spesific/Pagination.js
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 function Pagination(){
     const dispatch=useDispatch()
     return(
-<div className="h-[50px] max-[612px]:fixed bottom-0 p-[20px] w-full flex items-center rounded-b-[10px] justify-between bg-[#170b83dc]">
+<div className="h-[50px] max-[612px]:fixed bottom-0 p-[20px] w-full flex items-center rounded-b-[10px] max-[612px]:rounded-b-[0px] justify-between bg-[#170b83dc]">
     <Button title="< Prev" background="transparent" onClick={()=>{dispatch(decreaseSkip())}}/>
     <Button title="Next >" background="transparent" onClick={()=>{dispatch(increaseSkip())}}/>
 </div>
